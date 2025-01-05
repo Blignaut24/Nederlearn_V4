e25438bf469d01297d5f79073f8914e228820110
Johann Blignaut <blignaut24@gmail.com>
2025-01-05 13:58:33 +0000

    chore: prepare app for deployment with procfile and settings config

--- nederlearn/settings.py
+++ nederlearn/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-fqm8hrf&rz)j2uq(b%mq(y3v_^*(=y6kk(w_1-%3dhzn(k&dat"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
